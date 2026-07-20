bfb406a9482bed38f06fefb599d28f5bc207a373
Иван Шихалев <shikhalev@gmail.com>
2026-07-20 05:10:21 +0500

    Пн 20 июл 2026 05:10:21 +05

--- lib/jekyll-is/images/kramdown/converters.rb
+++ lib/jekyll-is/images/kramdown/converters.rb
@@ -1,1 +1,23 @@
 # frozen_string_literal: true
+
+require "kramdown"
+
+class Kramdown::Converter::Html
+  def convert_is_image(el, _opts)
+    el.value&.to_html
+  end
+
+  def convert_is_figure(el, _opts)
+    el.value&.to_html
+  end
+end
+
+class Kramdown::Converter::Latex
+  def convert_is_image(el, _opts)
+    el.value&.to_latex
+  end
+
+  def convert_is_figure(el, _opts)
+    el.value&.to_latex
+  end
+end
